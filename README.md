24656a176c513e9e0182945dd20a0fc27ac3f977
radarsu <rkrohmorath@gmail.com>
2017-01-03 23:57:42 +0100

    added paletting to overcome windows cli length limit

---
+++ README.md
@@ -0,0 +1,21 @@
+# tile-transition-combiner
+
+### Usage
+- Clone this project,
+- Install graphicsmagick + ts-node,
+- `ts-node tile-merger`
+
+### Input
+![Tile 1](https://github.com/radarsu/tile-transition-combiner/tiles/1.png)
+![Tile 2](https://github.com/radarsu/tile-transition-combiner/tiles/2.png)
+![Tile 3](https://github.com/radarsu/tile-transition-combiner/tiles/3.png)
+![Tile 4](https://github.com/radarsu/tile-transition-combiner/tiles/4.png)
+![Tile 5](https://github.com/radarsu/tile-transition-combiner/tiles/5.png)
+![Tile 6](https://github.com/radarsu/tile-transition-combiner/tiles/6.png)
+![Tile 6](https://github.com/radarsu/tile-transition-combiner/tiles/7.png)
+![Tile 7](https://github.com/radarsu/tile-transition-combiner/tiles/8.png)
+![Tile 8](https://github.com/radarsu/tile-transition-combiner/tiles/9.png)
+![Tile 9](https://github.com/radarsu/tile-transition-combiner/tiles/10.png)
+
+### Input
+![Tileset Output](https://github.com/radarsu/tile-transition-combiner/tileset-out.png)
--- tile-merger.ts
+++ tile-merger.ts
@@ -8,24 +8,23 @@ const exec = child_process.exec;
 const tilesDir = "./tiles";
 const tilesResizedDir = "./tiles-resized";
 const tilesOutDir = "./tiles-out";
+const tilePalettesDir = "./tile-palettes";
 const masksDir = "./masks";
 const testMask = "./mask.png";
 
 const tileColumns = 3;
 const tileSize = 64;
+const maxPalette = 510;
 const halfTileSize = tileSize / 2;
-
-const tileSet: any = {};
 
 const compositeMask = (png1: string, png2: string, mask: string, caseNumber: number) => {
     return new Promise((resolve, reject) => {
-        let gmComposite = `gm composite ${path.join(tilesResizedDir, png1)} ${path.join(tilesResizedDir, png2)} ${path.join(masksDir, mask)} ${path.join(tilesOutDir, png1.slice(0, -4))}-${png2.slice(0, -4)}-case-${caseNumber}.png`;
-        exec(gmComposite, function(err) {
+        const gmComposite = `gm composite ${path.join(tilesResizedDir, png1)} ${path.join(tilesResizedDir, png2)} ${path.join(masksDir, mask)} ${path.join(tilesOutDir, png1.slice(0, -4))}-${png2.slice(0, -4)}-case-${caseNumber}.png`;
+        exec(gmComposite, (err) => {
             if (err) {
                 throw err;
             }
 
-            // pathUpdate(entryID, { thumb: thumb });
             resolve();
         });
     });
@@ -36,11 +35,12 @@ const nameCase = (imageName: string, caseNumber: number) => {
 };
 
 const cleanTmpDirectories = async () => {
-    await fs.removeAsync(tilesResizedDir);
-    await fs.removeAsync(tilesOutDir);
+    const cleanDirs = [tilesResizedDir, tilesOutDir, tilePalettesDir];
 
-    await fs.mkdirAsync(tilesResizedDir);
-    await fs.mkdirAsync(tilesOutDir);
+    for (const dir of cleanDirs) {
+        await fs.removeAsync(dir);
+        await fs.mkdirAsync(dir);
+    }
 };
 
 const resizeTile = (png: string) => {
@@ -100,7 +100,19 @@ const generateCases = async () => {
     }
 };
 
-const joinImagesToTileset = async () => {
+const joinPalette = (gmState: any, paletteNumber: number) => {
+    return new Promise((resolve, reject) => {
+        gmState.mosaic().write(path.join(tilePalettesDir, `${paletteNumber}.png`), (err: Error) => {
+            if (err) {
+                return reject(err);
+            }
+
+            resolve();
+        });
+    });
+};
+
+const joinImagesToPalettes = async () => {
     const files = await fs.readdirAsync(tilesOutDir);
 
     const pngFiles = _.sortBy(_.sortBy(_.sortBy(_.filter(files, (object) => {
@@ -108,39 +120,82 @@ const joinImagesToTileset = async () => {
     }), (object) => {
         return +object.split(".")[0].split("-")[3];
     }), (object) => {
-        return +object.split(".")[0].split("-")[1];
-    }), (object) => {
         return +object.split(".")[0].split("-")[0];
+    }), (object) => {
+        return +object.split(".")[0].split("-")[1];
     });
 
-    console.log(`sorted`, pngFiles);
+    let gmState = (<any>gm)();
+    let maxCounter = 0;
+    let paletteNumber = 0;
+    for (const attr in pngFiles) {
+        const png = pngFiles[attr];
 
-    const gmState = (<any>gm)();
-    _.each(pngFiles, (png, i: number) => {
-        const row = Math.floor(i / tileColumns);
-        const column = i % tileColumns;
+        const row = Math.floor(maxCounter / tileColumns);
+        const column = maxCounter % tileColumns;
         gmState.in('-page', `+${column * tileSize}+${row * tileSize}`).in(path.join(tilesOutDir, png));
+
+        ++maxCounter;
+
+        if (maxCounter === maxPalette) {
+            await joinPalette(gmState, paletteNumber++);
+            // prepare for new palette
+            gmState = (<any>gm)();
+            maxCounter = 0;
+        }
+    }
+
+    if (maxCounter > 0) {
+        await joinPalette(gmState, paletteNumber);
+    }
+};
+
+const joinPalettesToTileset = async () => {
+    const files = await fs.readdirAsync(tilePalettesDir);
+
+    const paletteFiles = _.sortBy(_.filter(files, (object) => {
+        return object.split(".")[1] === "png";
+    }), (object) => {
+        return +object.split(".")[0];
+    });
+
+    let maxCounter = 0;
+    let paletteNumber = 0;
+    const gmState = (<any>gm)();
+    _.each(paletteFiles, (png, i: number) => {
+        console.log(png);
+        // gmState.in('-page', `+0+${i * tileSize * maxCounter / 3}`).in(path.join(tilePalettesDir, png));
+        gmState.append(path.join(tilePalettesDir, png));
     });
 
     return new Promise((resolve, reject) => {
-        gmState.mosaic().write("./tileset-out.png", (err: Error) => {
+        console.log(gmState);
+        gmState.write("./tileset-out.png", (err: Error) => {
+            if (err) {
+                return reject(err);
+            }
+
             resolve();
         });
     });
 };
 
 const run = async () => {
-    console.log(`${_.upperFirst("cleaning")}.`);
+    console.log(`${_.upperFirst("cleaning")}...`);
     await cleanTmpDirectories();
 
-    console.log(`${_.upperFirst("resizing")}.`);
+    console.log(`${_.upperFirst("resizing")}...`);
     await resizeTiles();
 
-    console.log(`${_.upperFirst("generating")}.`);
+    console.log(`${_.upperFirst("generating")}...`);
     await generateCases();
 
-    console.log(`${_.upperFirst("joining")}.`);
-    await joinImagesToTileset();
+    console.log(`${_.upperFirst("joining into palettes")}...`);
+    await joinImagesToPalettes();
+
+    console.log(`${_.upperFirst("joining into tileset")}...`);
+    await joinPalettesToTileset();
+
     console.log(`${_.upperFirst("done")}.`);
 };
 
